981d6c037c4f83d4ee2c4f4698a6768fc644691e
xxylu <80449534+xxylu@users.noreply.github.com>
2024-01-19 23:38:01 +0100

    poprawienie funkcji rand()

--- kolos II/zad1.cpp
+++ kolos II/zad1.cpp
@@ -12,13 +12,13 @@ float fun_aux(float a, float b, float c){
 }
 
 int main(){
-    srand(time (NULL));
-    int n = 4;
+    srand(time(NULL));
+    int n = 10;
     float* arr = new float[3*n];
     float* arr2 = new float[n];
     std::cout<<"Input: ";
     for(int i = 0; i < 3*n; i++){
-        arr[i] = rand() % 100;
+        arr[i] = rand() % 200 * 0.97;
         std::cout<<arr[i] << ", ";
     }
 
